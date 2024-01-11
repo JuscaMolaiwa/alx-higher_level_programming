a5cd254be231f07a0e0000f848eaa575237de5d9
JuscaMolaiwa <jusca.mol@gmail.com>
2024-01-10 23:41:33 -0800

    calculations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,11 @@ from calculator_1 import add, subtract, multiply, divide
 if __name__ == "__main__":
     a = 10
     b = 5
+    result_add = add(a, b)
+    result_subtract = subtract(a, b)
+    result_multiply = multiply(a, b)
+    result_divide = divide(a, b)
+
     print("{} + {} = {}".format(a, b, result_add))
     print("{} - {} = {}".format(a, b, result_subtract))
     print("{} * {} = {}".format(a, b, result_multiply))
