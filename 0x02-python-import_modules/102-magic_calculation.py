977ac2c15229117a2d765fd1ec7fa062aa5a1863
JuscaMolaiwa <jusca.mol@gmail.com>
2024-01-11 12:00:46 +0000

    advanced tasks

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).__dict__.values()
+
     if a < b:
         c = add(a, b)
+
         for i in range(4, 6):
             c = add(c, i)
+
         return c
+
     return sub(a, b)
