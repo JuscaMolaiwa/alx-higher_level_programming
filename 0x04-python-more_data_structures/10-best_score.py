0905d5e0a9f9f6abaa6d20ecb32b2139be0541c9
JuscaMolaiwa <jusca.mol@gmail.com>
2024-01-12 08:38:19 +0000

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    
-    max_key = max(a_dictionary, key=a_dictionary.get)
-    return max_key
+    if a_dictionary is not None:
+        if a_dictionary:  # Check if the dictionary is not empty
+            max_score = max(a_dictionary.values())
+            for key, value in a_dictionary.items():
+                if value == max_score:
+                    return key
+    return None
