da37dd152c674a8f1d9e1af906567a856d4c18e0
JuscaMolaiwa <jusca.mol@gmail.com>
2024-01-24 17:10:02 +0000

    Python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 """Defines a class Square"""
 
+
 class Square:
-    """Class that defines a square with a private size attribute.
-    """
+    """Class that defines a square with a private size attribute."""
+
     def __init__(self, size=0):
         """Instantiates a square with an optional size.
 
@@ -11,7 +13,6 @@ class Square:
             size (int, optional): The size of the square. Defaults to 0.
         """
         self.__size = 0  # Default size
-
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
