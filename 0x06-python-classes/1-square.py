ba95c37a524baa4a447ad48802612f9eb0239362
JuscaMolaiwa <jusca.mol@gmail.com>
2024-01-24 17:06:06 +0000

    Python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 """Defines a class Square"""
 
+
 class Square:
-    """Class that defines a square with a private size attribute.
-    """
+    """Class defines square with a private size attribute """
+
     def __init__(self, size):
         """Instantiates a square with a given size.
 
