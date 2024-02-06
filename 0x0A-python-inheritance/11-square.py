916f2b6fb5d313c3aa2c9dbad170ce1ce35fd3f4
JuscaMolaiwa <jusca.mol@gmail.com>
2024-02-06 20:21:50 +0000

    python inheritacne

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to define a class Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Class that defines a square"""
+
+    def __init__(self, size):
+        """Method for initialized the attrubutes"""
+
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """rectangle area"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
