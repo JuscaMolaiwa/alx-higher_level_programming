ceac127ef9428f50a7eb2cf6c61605e6123eeb01
JuscaMolaiwa <jusca.mol@gmail.com>
2024-02-06 16:34:47 +0000

    input_output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,12 @@
 
 
 def append_write(filename="", text=""):
-    """Append a string to the end of a UTF8 text file and return the number of characters added."""
+    """Append a string to the end of a UTF8 text file.
+
+
+     and return the number of characters added."""
+
+
     with open(filename, "a", encoding="utf-8") as f:
         f.write(text)
         return len(text)
