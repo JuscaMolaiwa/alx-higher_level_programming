49674c7773fce2100b67ff62ba9bba5ebb0e5c85
JuscaMolaiwa <jusca.mol@gmail.com>
2024-02-06 16:52:02 +0000

    input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,13 @@
 ii#!/usr/bin/python3
 """A script to add command-line arguments to a list and save them to a JSON file."""
-
 import sys
 from my_json_functions import save_to_json_file, load_from_json_file
 
 if __name__ == "__main__":
-    # Importing the functions for saving and loading from JSON files
-
-    # Attempting to load existing items from the file
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        items = []  # If the file doesn't exist, start with an empty list
-
-    # Adding the command-line arguments to the list
+        items = []
     items.extend(sys.argv[1:])
-
-    # Saving the updated list to the file
     save_to_json_file(items, "add_item.json")
 
