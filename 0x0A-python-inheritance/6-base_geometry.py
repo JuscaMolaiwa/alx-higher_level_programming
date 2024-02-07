0a32b6a482295aa6d3dd3f025edaf500e9880d8a
JuscaMolaiwa <jusca.mol@gmail.com>
2024-02-07 18:27:17 +0000

    inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+===================================
 """Defines the BaseGeometry class."""
+===================================
+"""
+
 
 class BaseGeometry:
     """Represents the base geometry class."""
+
     @classmethod
     def area(self):
         """This method Calculates the area of the geometry."""
+
         raise Exception("area() method is not implemented yet")
