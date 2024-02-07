5dbeb9c32ccb490522cf50f7d0cfbec5246b755d
JuscaMolaiwa <jusca.mol@gmail.com>
2024-02-07 19:05:22 +0000

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""
-This module defines the MyList class, which is a subclass of the built-in list class.
-MyList provides additional functionality,
-including a method to print its elements sorted.
-"""
+"""Module that Defines an inherited MyList Class."""
+
 
 class MyList(list):
     """Custom list subclass with added functionality."""
-    pass
-    
+
     def print_sorted(self):
         """Prints the elements of the list in sorted order."""
-
-        print(sorted(list(self)))
-
+        print(sorted(self))
