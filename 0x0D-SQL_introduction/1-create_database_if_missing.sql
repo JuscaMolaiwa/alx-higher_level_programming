c7cfe2aff5216fefec22ba25783f8d3e6ed1eb20
JuscaMolaiwa <jusca.mol@gmail.com>
2024-03-13 08:00:42 -0700

    MySQL

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Attempt to create the database hbtn_0c_0  if it doesn't exist in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- List all tables in the specified database on my MySQL server.
+SHOW TABLE;
