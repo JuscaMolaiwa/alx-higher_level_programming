b2b88578f419d982575ed41b98fd4086aee87335
JuscaMolaiwa <jusca.mol@gmail.com>
2024-03-13 12:40:58 -0700

    more

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,2 @@
 -- Lists all cities with their corresponding state names from the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-LEFT JOIN states ON state.id = cities.state_id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
