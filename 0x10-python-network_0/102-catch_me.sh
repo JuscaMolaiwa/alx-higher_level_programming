b160cdd5f98622227176c985367d846a72612ea4
Jusca <jusca.mol@gmail.com>
2024-07-14 13:17:15 -0700

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+# Makes a request to trigger the server response "You got me!"
 
-# Send a PUT request to 0.0.0.0:5000/catch_me
 curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" http://0.0.0.0:5000/catch_me
