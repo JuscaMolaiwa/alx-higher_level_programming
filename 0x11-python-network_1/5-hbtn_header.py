18b2f128475ef1ffe862b2119235b3798249a449
Jusca <jusca.mol@gmail.com>
2024-07-14 13:59:31 -0700

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the
+Takes in a URL, sends a request to the URL, and displays the
 value of the X-Request-Id variable found in the header of the
-response using requests
+response using requests.
 """
+
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    """takes in a URL, sends a request to the URL and displays
+    """Takes in a URL, sends a request to the URL, and displays
     the value of the X-Request-Id variable found in the header
-    of the response using requests"""
-    r = requests.get(argv[1])
+    of the response using requests."""
     try:
-        r_id = r.headers['X-Request-Id']
-        print(r_id)
-    except:
-        pass
+        url = argv[1]
+        r = requests.get(url)
+        r.raise_for_status()  # Raise an exception for bad response status
+
+        r_id = r.headers.get('X-Request-Id')
+        if r_id:
+            print(r_id)
+        else:
+            print("No X-Request-Id found in the response headers.")
+    except requests.exceptions.RequestException as e:
+        print("Error occurred:", e)
