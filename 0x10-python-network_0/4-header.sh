544b81fc5ce6e7ed5a65c4613a20d27c8c03c6ae
Jusca <jusca.mol@gmail.com>
2024-07-14 12:08:50 -0700

    https

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+curl -sX GET $1 -H "X-School-User-Id: 98" -L
