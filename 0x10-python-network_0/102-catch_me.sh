0dbf2f956670b6b5e2fcbd3f66b71434e9ee14a4
Jusca <jusca.mol@gmail.com>
2024-07-14 13:15:04 -0700

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+
+# Send a PUT request to 0.0.0.0:5000/catch_me
+curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" http://0.0.0.0:5000/catch_me
