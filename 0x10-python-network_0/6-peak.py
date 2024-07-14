89a510731f554438bad395e473d09fddebb076b6
Jusca <jusca.mol@gmail.com>
2024-07-14 12:44:29 -0700

    fixes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
- Finds a peak (element greater than both neighbors) in an unsorted list of integers.
+Finds a peak (element greater than both neighbors) in an unsorted list of integers.
 
- Args:
+Args:
   list_of_integers: A list of integers.
 
- Returns:
+Returns:
   The first peak element found in the list, or None if no peak exists.
 """
+
 def find_peak(list_of_integers):
     """
     Finds a peak (element greater than both neighbors) in an unsorted list of integers.
@@ -20,15 +21,15 @@ def find_peak(list_of_integers):
     """
     if not list_of_integers:
         return None
-    
+
     def peak_helper(nums, left, right):
         if left == right:
             return nums[left]
-        
+
         mid = (left + right) // 2
         if nums[mid] > nums[mid + 1]:
             return peak_helper(nums, left, mid)
         else:
             return peak_helper(nums, mid + 1, right)
-    
+
     return peak_helper(list_of_integers, 0, len(list_of_integers) - 1)
